c74ba93bff9f24da2af7327388a0e0bf74b0b901
HamitMizrak <hamitmizrak@gmail.com>
2021-05-20 01:03:43 +0300

    first commit sts -3

---
+++ src/test/java/com/hamit/SpringBootExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
