404d0e566d8b750dccda84f2aeb8fa37b4af0bd1
HamitMizrak <hamitmizrak@gmail.com>
2021-05-24 18:21:24 +0300

    @NotNull BooksDto ve WriterDto eklendi.

--- src/main/java/com/hamit/dto/BooksDto.java
+++ src/main/java/com/hamit/dto/BooksDto.java
@@ -2,24 +2,29 @@ package com.hamit.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@NonNull // Lombok
+// @NotNull // validation
 public class BooksDto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long bookId;
-	
+	@NotNull
 	private String bookName;
-	
+	@NotNull
 	private String bookYearOfPrinting;
-	
+	@NotNull
 	private String bookVersion;
-	
+	@NotNull
 	private String bookPrintingHouse;
 	
 	private WriterDto writerEntity;
--- src/main/java/com/hamit/dto/WriterDto.java
+++ src/main/java/com/hamit/dto/WriterDto.java
@@ -3,6 +3,8 @@ package com.hamit.dto;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,21 +12,22 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+// @Builder
 public class WriterDto implements Serializable {
 	
 	private static final long serialVersionUID = -9190714480247693530L;
 	
 	private Long writerId;
-	
+	@NotNull
 	private String writerName;
-	
+	@NotNull
 	private String writerSurname;
-	
+	@NotNull
 	private String writerAge;
-	
+	@NotNull
 	private String writerAddress;
-	
+	@NotNull
 	private String writerSocialMedia;
-	
+	@NotNull
 	private List<BooksDto> booksEntities;
 }
