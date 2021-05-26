2eb3828c0898eff9ab6e789e04cf987a31396b19
HamitMizrak <hamitmizrak@gmail.com>
2021-05-26 20:15:03 +0300

    Postman

---
+++ src/main/java/com/hamit/posttman/PostmanUrl.java
@@ -0,0 +1,9 @@
+package com.hamit.posttman;
+
+public class PostmanUrl {
+	// GET: http://localhost:9292/api/writer/get/chooise/10
+	// POST: http://localhost:9292/api/writer/post
+	// DELETE: localhost:9292/api/writer/delete/9
+	// LİST: localhost:9292/api/writer/get/list
+	// PUT: localhost:9292/api/writer/update/10
+}
