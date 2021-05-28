37f41f226f3aa150b86e62237f476b3ccb944ca9
HamitMizrak <hamitmizrak@gmail.com>
2021-05-28 23:12:29 +0300

    Jackson - Json eklendi. Manuel (data.json,data.sql)

--- src/main/java/com/hamit/SpringBootExamplesApplication.java
+++ src/main/java/com/hamit/SpringBootExamplesApplication.java
@@ -4,8 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @SpringBootApplication
+//@EnableSwagger2
 public class SpringBootExamplesApplication {
 	
 	public static void main(String[] args) {
@@ -16,5 +20,18 @@ public class SpringBootExamplesApplication {
 	public ModelMapper getModelMapper() {
 		return new ModelMapper();
 	}
+	
+@Bean
+public Jackson2RepositoryPopulatorFactoryBean jackson2RepositoryPopulatorFactoryBean() {
+	
+	//package org.springframework.core.io;
+	
+	Resource json1 = (Resource) new ClassPathResource("manuel_writer.json");
+    Resource json2 = (Resource) new ClassPathResource("writer.json");
+	
+	Jackson2RepositoryPopulatorFactoryBean factoryBean=new Jackson2RepositoryPopulatorFactoryBean();
+	factoryBean.setResources(new Resource[]{json1,json2});
+	return factoryBean;
+}
 	
 }
---
+++ src/main/resources/manuel_query.sql
@@ -0,0 +1,1 @@
+NSERT INTO `springbookstore`.`writer` (`writer_name`, `writer_surname`) VALUES ('Talha-2', 'sekeroglu-2');
